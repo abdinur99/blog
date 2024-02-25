8c2217594cbfde50a2f346a87d9fb155cee9e7e0
Abdi <malikalshebaz@gmail.com>
2024-02-25 22:21:22 +0300

    added search feature

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [ 
+        'posts' => $category->posts->load(['category', 'author']),
+        'categories' => Category::all(),
+        'currentCategory' => $category,
+    ]);
+})->name('category');
+
+Route::get('authors/{author:username}', function(User $author){
+    return View(
+        'posts', [
+            'posts' => $author->posts,
+            'categories' => Category::all(),  
+        ]
+    );
 });
